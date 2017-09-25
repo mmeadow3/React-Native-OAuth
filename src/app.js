0b502e536fbad6360824c372471d7913be7e60d1
Mike Meadows <mmeadow33@gmail.com>
2017-09-25 13:50:29 -0500

    make an app file so ios and andriod both work

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+Class App extends Component => {
+  render(){
+    return (
+      <Text>an App</Text>
+    );
+  }
+}
+
+export default App;
